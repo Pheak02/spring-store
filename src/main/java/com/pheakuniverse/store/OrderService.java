6085915433f22b368a7713c82f1d2f163ba6347e
Pheak02 <saingsopheak02@gmail.com>
2026-03-22 14:11:24 +0700

    task:Spring IOC container https://youtu.be/gJrjgg1KVL4?t=3794

--- src/main/java/com/pheakuniverse/store/OrderService.java
+++ src/main/java/com/pheakuniverse/store/OrderService.java
@@ -3,9 +3,9 @@ package com.pheakuniverse.store;
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) { //this act like setter
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService) { //this act like setter
+        this.paymentService = paymentService;
+    }
     
     public void placeOrder() {
 //        var stripePaymentService = new StripePaymentService();
