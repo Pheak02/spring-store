381cb54ea805642c84d36a565b302cdfd7cb3290
Pheak02 <saingsopheak02@gmail.com>
2026-03-22 10:28:00 +0700

    task:[Payment] extract interface

---
+++ src/main/java/com/pheakuniverse/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.pheakuniverse.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- src/main/java/com/pheakuniverse/store/StripePaymentService.java
+++ src/main/java/com/pheakuniverse/store/StripePaymentService.java
@@ -1,7 +1,8 @@
 package com.pheakuniverse.store;
 
-public class StripePaymentService {
-    public void processPayment(double amount){
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
         System.out.println("Stripe");
         System.out.println("Amount:" + amount);
     }
