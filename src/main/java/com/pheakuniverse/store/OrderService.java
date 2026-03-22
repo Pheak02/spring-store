6d446833107da1b61d92093b44a681132da07087
Pheak02 <saingsopheak02@gmail.com>
2026-03-22 14:21:41 +0700

    task:config BEAN using annotation https://youtu.be/gJrjgg1KVL4?t=4258

--- src/main/java/com/pheakuniverse/store/OrderService.java
+++ src/main/java/com/pheakuniverse/store/OrderService.java
@@ -1,8 +1,15 @@
 package com.pheakuniverse.store;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
+//    @Autowired //this annotation cuz not to auto default constructor
     public OrderService(PaymentService paymentService) { //this act like setter
         this.paymentService = paymentService;
     }
